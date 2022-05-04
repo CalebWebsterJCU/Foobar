398672edac5384ae50ecabf739acb34cab97a55d
CalebWebsterJCU <caleb.webster@my.jcu.edu.au>
2022-05-04 23:41:10 +1000

    Complete challenge

---
+++ 1TheCakeIsNotALie/src/com/webster/caleb/foobar/thecakeisnotalie/Main.java
@@ -0,0 +1,9 @@
+package com.webster.caleb.foobar.thecakeisnotalie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "abcabababababcabababab";
+        System.out.println(Solution.getMaxPieces(str, 1));
+    }
+}
